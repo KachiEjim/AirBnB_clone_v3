26e96457e602a32cf7502f8002278d5e3a6983fd
root <kachyejim>
2024-05-25 21:33:49 +0000

    Fixing conflicts

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,7 +16,7 @@ app.register_blueprint(app_views)
 def page_not_found(error):
     """Handles error 404 file not found"""
 
-    return jsonify({"error": "Not found"})
+    return jsonify({'error': 'Not found'})
 
 
 @app.teardown_appcontext
